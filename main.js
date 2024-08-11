42257793a571e5719747444c122015bca08e7c5c
Vignesh G <107738847+Sweety-Vigneshg@users.noreply.github.com>
2024-08-11 13:31:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const eventDate = new Date("Jan 1, 2025 00:00:00").getTime();
+const countdown = setInterval(function() {
+    const now = new Date().getTime();
+    const timeLeft = eventDate - now;
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if (timeLeft < 0) {
+        clearInterval(countdown);
+        document.getElementById("timer").innerHTML = "Event Started!";
+    }
+
+}, 1000);
